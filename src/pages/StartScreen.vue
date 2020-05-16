a99125cb867020481c918ff8725a0b18a50ea2e2
NatanCieplinski <natan.cieplinski.developer@gmail.com>
2020-05-16 22:43:45 +0200

    First route added

---
+++ src/pages/StartScreen.vue
@@ -0,0 +1,12 @@
+<template>
+   <div class="start-screen fullscreen"></div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="sass" scoped>
+.start-screen
+   background-color: #323232
+</style>
--- src/router/router.js
+++ src/router/router.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import StartScreen from '@/pages/StartScreen.vue'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+   {
+      path: '/',
+      name: 'start-screen',
+      component: StartScreen
+   }
+]
 
 const router = new VueRouter({
    mode: 'history',
