821a8ae354ba577b048d5c69dd8c1f37553bade9
NatanCieplinski <natan.cieplinski.developer@gmail.com>
2020-05-22 00:35:23 +0200

    Add party code for host

--- src/pages/PartySetup.vue
+++ src/pages/PartySetup.vue
@@ -1,14 +1,18 @@
 <template>
    <div class="party-setup fullscreen">
       <h1>Logged in correctly</h1>
+      <h2>Il codice del tuo party è {{ party.party_code }}</h2>
    </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapState, mapActions } from 'vuex'
 import Utils from '@/utils.js'
 
 export default {
+   computed: {
+      ...mapState('party', ['party'])
+   },
    methods: {
       ...mapActions('user', ['setToken', 'setUser']),
       ...mapActions('party', ['createParty'])
@@ -26,6 +30,7 @@ export default {
 <style lang="sass" scoped>
 .party-setup
    display: flex
+   flex-direction: column
    justify-content: center
    align-items: center
 </style>
