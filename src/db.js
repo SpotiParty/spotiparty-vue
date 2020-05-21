562976d9cc4d569b5b817b45c7520447c92bcced
NatanCieplinski <natan.cieplinski.developer@gmail.com>
2020-05-21 20:23:15 +0200

    Firebase configuration added

---
+++ src/db.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+export const db = firebase
+   .initializeApp({ projectId: 'spotiparty-a24ed' })
+   .firestore()
+
+const { TimeStamp, GeoPoint } = firebase.firestore
+export { TimeStamp, GeoPoint }
+
+db.settings({ timestampsInSnapshots: true })
