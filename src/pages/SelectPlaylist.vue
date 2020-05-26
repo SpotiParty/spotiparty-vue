f9d3f2ac692935afc30125c4b9e61ca819f6c95a
Patrizia Villani <patrizia.vil@icloud.com>
2020-05-26 19:36:14 +0200

    Initial styling  added to SelectPlaylist

--- src/pages/SelectPlaylist.vue
+++ src/pages/SelectPlaylist.vue
@@ -1,6 +1,6 @@
 <template>
-   <div>
-      <h1>Pagina SelectPlaylist</h1>
+   <div class="select-playlist fullscreen">
+      <p class="title">Scegli una playlist</p>
    </div>
 </template>
 
@@ -8,4 +8,19 @@
 export default {}
 </script>
 
-<style lang="sass" scoped></style>
+<style lang="sass" scoped>
+.select-playlist
+   align-items: left
+   background-color: #323232
+   box-sizing: border-box
+   display: flex
+   flex-direction: column
+   justify-content: space-between
+   padding: 30px
+.title
+   text-align: left
+   font-size: 32px
+   font-weight: 600
+   color: white
+   margin: 0px
+</style>
