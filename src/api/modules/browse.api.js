881b05e4f596fd013c5f3c20c7c2299491d07f65
Natan Cieplinski <natan.cieplinski.developer@gmail.com>
2020-05-28 04:45:42 +0200

    Spotify browse APIs (#7)

--- src/api/modules/browse.api.js
+++ src/api/modules/browse.api.js
@@ -1,10 +1,12 @@
 import apiClient from '@/api/apiClient.js'
 
 export default {
-   getListOfCategory() {
-      return apiClient.get('/browse/categories')
+   getListOfCategory(offset, limit) {
+      return apiClient.get(`/browse/categories?offset=${offset}&limit=${limit}`)
    },
    getListOfCategoryPlaylists(category_id) {
-      return apiClient.get(`/browse/categories/${category_id}/playlists`)
+      return apiClient.get(
+         `/browse/categories/${category_id}/playlists?limit=${50}`
+      )
    }
 }
---
+++ src/store/modules/browse.store.js
@@ -0,0 +1,55 @@
+import BrowseApi from '@/api/modules/browse.api.js'
+
+export default {
+   namespaced: true,
+   state: {
+      categories: [],
+      category_playlists: [],
+      offset: 0,
+      limit: 8
+   },
+   mutations: {
+      ADD_CATEGORIES(state, categories) {
+         state.offset += state.limit
+         state.categories.concat(categories)
+      },
+      ADD_CATEGORY_PLAYLISTS(state, params) {
+         state.category_playlists.push(params)
+      }
+   },
+   actions: {
+      async getCategories({ state, commit }) {
+         //TODO api call when scroll
+         await BrowseApi.getListOfCategory(state.offset, state.limit)
+            .then(response => {
+               commit('ADD_CATEGORIES', response.data.items)
+            })
+            .catch(error => console.log(error))
+      },
+      async getCategoryPlaylists({ commit }, category_id) {
+         await BrowseApi.getListOfCategoryPlaylists(category_id)
+            .then(response => {
+               const params = {
+                  category_id: category_id,
+                  playlists: response.data.items
+               }
+               commit('ADD_CATEGORY_PLAYLISTS', params)
+            })
+            .catch(error => console.log(error))
+      }
+   },
+   getters: {
+      category_has_playlists: state => category_id => {
+         return (
+            state.category_playlists.find(
+               item => item.category_id == category_id
+            ) != undefined
+         )
+      },
+      category_playlists: state => category_id => {
+         return state.category_playlists.find(
+            item => item.category_id == category_id
+         ).playlists
+      }
+   }
+}
