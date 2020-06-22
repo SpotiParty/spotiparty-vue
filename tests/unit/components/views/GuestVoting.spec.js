3b8da5a70a1a1cf7d69364ab17f2ab39d222eb7f
faridevnz <alessio.farinelli.developer@gmail.com>
2020-06-22 06:19:13 +0200

    added test for GuestVoting view

---
+++ tests/unit/components/views/GuestVoting.spec.js
@@ -0,0 +1,86 @@
+import { mount, shallowMount, createLocalVue } from '@vue/test-utils'
+import GuestVoting from '@/components/views/GuestVoting.vue'
+import Song from '@/components/Song.vue'
+import BattleVote from '@/components/BattleVote.vue'
+import Vuex from 'vuex'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+// store
+const party = {
+   namespaced: true,
+   state: {
+      party_playlist: {
+         tracks: [
+            {
+               id: '0',
+               played: false,
+               images: [{ url: 'image/url' }],
+               artists: [{ name: 'artist_name' }]
+            },
+            {
+               id: '1',
+               played: false,
+               images: [{ url: 'image/url' }],
+               artists: [{ name: 'artist_name' }]
+            }
+         ],
+         name: '',
+         id: null,
+         uri: null
+      },
+      voted_song_id: '0',
+      party_mode: {
+         mode: 'democracy',
+         battle_songs: ['0', '1']
+      }
+   },
+   mutations: {},
+   actions: {},
+   getters: {}
+}
+const store = new Vuex.Store({
+   modules: { party }
+})
+// end store
+
+describe('Component', () => {
+   test('is a Vue instance', () => {
+      const wrapper = shallowMount(GuestVoting, {
+         localVue,
+         store
+      })
+      expect(wrapper.isVueInstance()).toBeTruthy()
+   })
+})
+
+describe('GuestVoting democracy mode', () => {
+   test('correct rendering of nested components', async () => {
+      const wrapper = mount(GuestVoting, {
+         localVue,
+         store
+      })
+      expect(wrapper.find(Song).exists()).toBe(true)
+      expect(wrapper.findAll(Song).length).toBe(2)
+      expect(wrapper.find(Song).attributes('class')).toContain('selected')
+   })
+})
+
+describe('GuestVoting battle mode', () => {
+   test('correct rendering of nested components', async () => {
+      store.state.party.party_mode.mode = 'battle'
+      const wrapper = mount(GuestVoting, {
+         localVue,
+         store
+      })
+      expect(wrapper.find(BattleVote).exists()).toBe(true)
+      expect(wrapper.findAll(BattleVote).length).toBe(2)
+      expect(
+         wrapper
+            .findAll(BattleVote)
+            .at(0)
+            .attributes('class')
+      ).toContain('selected')
+   })
+})
